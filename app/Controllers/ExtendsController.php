2c63d1426d2e34f6062ca84c82a07b83bea23382
raditya2010631170111 <2010631170111@student.unsika.ac.id>
2023-10-23 23:30:03 +0700

    -

---
+++ app/Controllers/ExtendsController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Controllers\BaseController;
+
+class ExtendsController extends BaseController {
+	
+	public function index() {
+        $data['nickname'] = "Radit";
+        return view('home', $data);
+    }
+	
+	// public function profile() { 
+    public function profile(): string {
+        $data['name'] = "Raditya Aji Sasmoyo";
+        $data['nickname'] = "Radit";
+        $data['npm'] = "2010631170111";
+        $data['class'] = "Framework Pemograman Web";
+        $data['phone'] = "+62 812-9026-4815";
+        $data['email'] = "2010631170111@student.unsika.ac.id";
+        $data['username'] = "raditya2010631170111";
+
+        return view('about_us', $data);
+		
+		// Using var_dump for debugging
+		var_dump($data);
+
+		$this->load->view('about_us', $data);
+	}
+}
